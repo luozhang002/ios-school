4c58e7477d94f3c4c2e5032226a1a11e20743a97
xiaoluo <xiaoluo@mogujie.com>
2016-08-16 11:21:31 +0800

    feat:add calculator program

---
+++ Caculator/Caculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Caculator
+//
+//  Created by 罗章 on 16/8/16.
+//  Copyright © 2016年 mogujie.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by 罗章 on 16/8/7.
+//  Copyright © 2016年 mogujieer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
